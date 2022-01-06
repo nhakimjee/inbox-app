3992eee908e72ea3e9d2001f1c595eca6ad795ca
Nuruddin Hakimjee <nhakimjee@singledigits.com>
2022-01-06 16:49:43 -0500

    Added Github OAuth

--- src/main/java/com/example/inbox/InboxAppApplication.java
+++ src/main/java/com/example/inbox/InboxAppApplication.java
@@ -1,13 +1,24 @@
 package com.example.inbox;
 
+import java.time.Clock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class InboxAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InboxAppApplication.class, args);
 	}
 
+	@RequestMapping("/user")
+	public String getUser(@AuthenticationPrincipal OAuth2User principal) {
+		System.out.println(principal);
+		return principal.getAttribute("name");
+	}
 }
