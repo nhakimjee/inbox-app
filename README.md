0726caaff485cdf3ba9c1769050a3d330912458c
Nuruddin Hakimjee <nhakimjee@singledigits.com>
2022-01-07 18:34:50 -0500

    Add Astra DB connection infrastructure

--- README.md
+++ README.md
@@ -7,3 +7,19 @@ Steps:
 3. Run the Spring Boot App. You should be able to login with GitHub
 
 Post login, you will be redirected back to the login page, but you can validate the authorized principal is created by accessing the `/user` API. 
+
+Spring Data Configuration:
+1. Generate client token for Datastax
+2. Substitute clientId for spring.data.cassandra.username
+3. Substitute clientSecret for spring.data.cassandra.password
+
+Datastax Database Configuration
+1. Set values for astra.db as follows:
+   1. id from db clusterId
+   2. reqion from db region
+   3. keyspace from the db keyspace
+   4. application.token from client token generated above
+
+2. Download secure-connect.zip for the datastax db from the connect tab
+   to the resources folder
+3. Put the secure connect bundle in the .gitignore so that it is not in github
--- src/main/java/com/example/inbox/InboxAppApplication.java
+++ src/main/java/com/example/inbox/InboxAppApplication.java
@@ -1,8 +1,11 @@
 package com.example.inbox;
 
+import java.nio.file.Path;
 import java.time.Clock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,16 @@ public class InboxAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InboxAppApplication.class, args);
+	}
+
+	/**
+	 * This is necessary to have Spring boot use the Astra secure bundle
+	 * to connect to the database
+	 */
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
 	}
 
 	@RequestMapping("/user")
